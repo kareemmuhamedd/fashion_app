f2f5047277504117cd6272e1c9e10bc90771ffe6
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-09-05 19:58:05 +0300

    - Added a custom_app_bar widget to home_screen. - Added a notification_widget with Badge counter. - Added an initial notification_screen. - Added an initial search_screen.

--- lib/common/utils/app_routes.dart
+++ lib/common/utils/app_routes.dart
@@ -1,6 +1,8 @@
 
 import 'package:fashion_app/src/auth/views/login_screen.dart';
 import 'package:fashion_app/src/entrypoint/views/entrypoint.dart';
+import 'package:fashion_app/src/notification/views/notification_screen.dart';
+import 'package:fashion_app/src/search/views/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -37,10 +39,10 @@ final GoRouter _router = GoRouter(
     //   path: '/verification',
     //   builder: (context, state) => const VerificationPage(),
     // ),
-    // GoRoute(
-    //   path: '/search',
-    //   builder: (context, state) => const SearchPage(),
-    // ),
+    GoRoute(
+      path: '/search',
+      builder: (context, state) => const SearchScreen(),
+    ),
     // GoRoute(
     //   path: '/help',
     //   builder: (context, state) => const HelpCenterPage(),
@@ -76,10 +78,10 @@ final GoRouter _router = GoRouter(
     //   builder: (context, state) => const AddressesListPage(),
     // ),
     //
-    //  GoRoute(
-    //   path: '/notifications',
-    //   builder: (context, state) => const NotificationPage(),
-    // ),
+     GoRoute(
+      path: '/notifications',
+      builder: (context, state) => const NotificationsScreen(),
+    ),
     //
     //  GoRoute(
     //   path: '/tracking',
--- lib/src/home/views/home_screen.dart
+++ lib/src/home/views/home_screen.dart
@@ -1,10 +1,16 @@
+import 'package:fashion_app/src/home/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(110),
+        child: CustomAppBar(),
+      ),
       body: Center(
         child: Text('Home Screen'),
       ),
