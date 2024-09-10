fb343cc7a356e494f34df1e344c2ecf28a1d98f9
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-09-10 04:05:59 +0300

    - Added authentication models. - Added AuthNotifier containing all authentication endpoints, including login, registration, and fetching user data. - Hooked up AuthNotifier in the LoginScreen and RegistrationScreen.

--- lib/src/profile/views/profile_screen.dart
+++ lib/src/profile/views/profile_screen.dart
@@ -5,12 +5,17 @@ import 'package:fashion_app/common/widgets/app_style.dart';
 import 'package:fashion_app/common/widgets/custom_button.dart';
 import 'package:fashion_app/common/widgets/help_bottom_sheet.dart';
 import 'package:fashion_app/common/widgets/reusable_text.dart';
+import 'package:fashion_app/src/auth/controllers/auth_notifier.dart';
+import 'package:fashion_app/src/auth/models/profile_model.dart';
 import 'package:fashion_app/src/auth/views/login_screen.dart';
 import 'package:fashion_app/src/profile/widget/profile_tile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../../entrypoint/controllers/bottom_tab_notifier.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -22,98 +27,108 @@ class ProfileScreen extends StatelessWidget {
       return const LoginScreen();
     }
     return Scaffold(
-      body: ListView(
-        children: [
-          Column(
+      body: Consumer<AuthNotifier>(
+        builder: (context, authNotifier, child) {
+          ProfileModel? user = authNotifier.getUserData();
+          return ListView(
             children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  const CircleAvatar(
+                    radius: 35,
+                    backgroundColor: Kolors.kOffWhite,
+                    backgroundImage: NetworkImage(AppText.kProfilePic),
+                  ),
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                  ReusableText(
+                    text: user!.email,
+                    style: appStyle(
+                      11,
+                      Kolors.kGray,
+                      FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7.h,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15.w),
+                    decoration: BoxDecoration(
+                      color: Kolors.kOffWhite,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ReusableText(
+                      text: user.username,
+                      style: appStyle(
+                        14,
+                        Kolors.kDark,
+                        FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
+              ),
               SizedBox(
                 height: 30.h,
               ),
-              const CircleAvatar(
-                radius: 35,
-                backgroundColor: Kolors.kOffWhite,
-                backgroundImage: NetworkImage(AppText.kProfilePic),
-              ),
-              SizedBox(
-                height: 15.h,
-              ),
-              ReusableText(
-                text: 'karmoh@gmail.com',
-                style: appStyle(
-                  11,
-                  Kolors.kGray,
-                  FontWeight.normal,
-                ),
-              ),
-              SizedBox(
-                height: 7.h,
-              ),
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 15.w),
-                decoration: BoxDecoration(
-                  color: Kolors.kOffWhite,
-                  borderRadius: BorderRadius.circular(10),
+                color: Kolors.kOffWhite,
+                child: Column(
+                  children: [
+                    ProfileTileWidget(
+                      title: 'My Orders',
+                      leading: Octicons.checklist,
+                      onTap: () {
+                        context.push('/orders');
+                      },
+                    ),
+                    ProfileTileWidget(
+                      title: 'Shipping Address',
+                      leading: MaterialIcons.location_pin,
+                      onTap: () {
+                        context.push('/addresses');
+                      },
+                    ),
+                    ProfileTileWidget(
+                      title: 'Privacy Policy',
+                      leading: MaterialIcons.policy,
+                      onTap: () {
+                        context.push('/policy');
+                      },
+                    ),
+                    ProfileTileWidget(
+                      title: 'Help Center',
+                      leading: AntDesign.customerservice,
+                      onTap: () => showHelpCenterBottomSheet(context),
+                    ),
+                  ],
                 ),
-                child: ReusableText(
-                  text: 'Kareem Muhamed',
-                  style: appStyle(
-                    14,
-                    Kolors.kDark,
-                    FontWeight.w600,
-                  ),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 14),
+                child: CustomButton(
+                  text: 'Logout'.toUpperCase(),
+                  btnColor: Kolors.kRed,
+                  btnHieght: 35,
+                  btnWidth: ScreenUtil().screenWidth,
+                  onTap: () {
+                    Storage().removeKey('accessToken');
+                    context.read<TabIndexNotifier>().setIndex(0);
+                    context.go('/home');
+                  },
                 ),
               )
             ],
-          ),
-          SizedBox(
-            height: 30.h,
-          ),
-          Container(
-            color: Kolors.kOffWhite,
-            child: Column(
-              children: [
-                ProfileTileWidget(
-                  title: 'My Orders',
-                  leading: Octicons.checklist,
-                  onTap: () {
-                    context.push('/orders');
-                  },
-                ),
-                ProfileTileWidget(
-                  title: 'Shipping Address',
-                  leading: MaterialIcons.location_pin,
-                  onTap: () {
-                    context.push('/addresses');
-                  },
-                ),
-                ProfileTileWidget(
-                  title: 'Privacy Policy',
-                  leading: MaterialIcons.policy,
-                  onTap: () {
-                    context.push('/policy');
-                  },
-                ),
-                ProfileTileWidget(
-                  title: 'Help Center',
-                  leading: AntDesign.customerservice,
-                  onTap: () => showHelpCenterBottomSheet(context),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 14),
-            child: CustomButton(
-              text: 'Logout'.toUpperCase(),
-              btnColor: Kolors.kRed,
-              btnHieght: 35,
-              btnWidth: ScreenUtil().screenWidth,
-            ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
