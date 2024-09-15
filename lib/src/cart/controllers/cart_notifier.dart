072e41dc41dfae227e33594fe4e6c400f32f972e
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-09-15 21:08:18 +0300

    - Added ListView in CartScreen to fetch all items in the user's cart. - Added CartNotifier to handle functionality for incrementing and decrementing the quantity of specific products. - Added fetch_cart and fetch_cart_count hooks. - Added cart_count_results and cart_results. - Added cart_count_model, cart_model, and create_cart_model. - Added cart_counter, cart_tile, and update button widgets.

---
+++ lib/src/cart/controllers/cart_notifier.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+
+class CartNotifier with ChangeNotifier {
+  int _qty = 0;
+
+  int get qty => _qty;
+
+  void increment() {
+    _qty++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    if (_qty > 1) {
+      _qty--;
+      notifyListeners();
+    }
+  }
+
+  int? _selectedCart;
+
+  int? get selectedCart => _selectedCart;
+
+  void setSelectedCounter(int id, int q) {
+    _selectedCart = id;
+    _qty = q;
+    notifyListeners();
+  }
+
+  void clearSelectedCart() {
+    _selectedCart = null;
+    _qty = 0;
+    notifyListeners();
+  }
+}
--- lib/src/cart/views/cart_screen.dart
+++ lib/src/cart/views/cart_screen.dart
@@ -1,19 +1,56 @@
+import 'package:fashion_app/src/cart/widgets/cart_tile.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../common/services/storage.dart';
+import '../../../common/utils/kcolors.dart';
+import '../../../common/utils/kstrings.dart';
+import '../../../common/widgets/app_style.dart';
+import '../../../common/widgets/reusable_text.dart';
+import '../../../common/widgets/shimmers/list_shimmer.dart';
 import '../../auth/views/login_screen.dart';
-class CartScreen extends StatelessWidget {
+import '../hooks/fetch_cart.dart';
+
+class CartScreen extends HookWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     String? accessToken = Storage().getString('accessToken');
+    final results = fetchCart();
+    final carts = results.cart;
+    final isLoading = results.isLoading;
+    final refetch = results.refetch;
+    final error = results.error;
     if (accessToken == null) {
       return const LoginScreen();
     }
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Screen'),
+    if (isLoading) {
+      return const Scaffold(
+        body: ListShimmer(),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: ReusableText(
+          text: AppText.kCart,
+          style: appStyle(
+            15,
+            Kolors.kPrimary,
+            FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding:  EdgeInsets.symmetric(horizontal: 12.w),
+        children: List.generate(
+          carts.length,
+          (index) {
+            final cart = carts[index];
+            return CartTile(cart: cart);
+          },
+        ),
       ),
     );
   }
